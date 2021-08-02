762855767f3228ffcb0ff01921329dc52e2fb968
Arseniy Titov <70908119+famaxth@users.noreply.github.com>
2021-08-02 22:30:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
-# - *- coding: utf- 8 - *-
-
-#Production by Famaxth
-#Telegram - @por0vos1k
-
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
 options.headless = True
 
-path = 'path/to/chromedriver.exe' #you need to change this
+path = "path/to/chromedriver.exe"  # You need to change this
+
 
 def parser():
     driver = webdriver.Chrome(path, chrome_options=options)
     driver.get('https://yandex.ru/pogoda/213')
-    weather = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[2]/div[1]/div[5]/a/div[1]/span[2]').text
+    weather = driver.find_element_by_xpath(
+        '/html/body/div[1]/div[3]/div[2]/div[1]/div[5]/a/div[1]/span[2]').text
     city_full_text = driver.find_element_by_xpath('//*[@id="main_title"]').text
-    term_value = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[2]/div[1]/div[7]/div[2]/div[2]').text
-
+    term_value = driver.find_element_by_xpath(
+        '/html/body/div[1]/div[3]/div[2]/div[1]/div[7]/div[2]/div[2]').text
     city = city_full_text.replace("е", "а")
+    print(
+        f'Город: {city.replace("Погода в ", "")}\nТемпература: {weather}°\nВлажность: {term_value}')
 
-    print(f'Город: {city.replace("Погода в ", "")}\nТемпература: {weather}°\nВлажность: {term_value}')
 
 parser()
